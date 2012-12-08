aaeec3de58b7b5407ef6b57f6f22f139e4e011df
jeremydmiller <jeremydmiller@yahoo.com>
2012-12-08 12:55:15 -0600

    kinda, sorta got the page level ToC working

---
+++ src/FubuWorld/Navigation/FubuWorldPageExtensions.cs
@@ -0,0 +1,13 @@
+﻿using FubuMVC.Core.View;
+using HtmlTags;
+
+namespace FubuWorld.Navigation
+{
+    public static class FubuWorldPageExtensions
+    {
+         public static HtmlTag SectionFor(this IFubuPage page, string text, string id)
+         {
+             return new HtmlTag("section", tag => tag.Add("h4").Text(text).AddClass("section-header")).Id(id).NoClosingTag();
+         }
+    }
+}
